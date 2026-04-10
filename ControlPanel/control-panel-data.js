335bbcef2201473f79e7574d93e21e0cc1068b81
jrpooler <74581872+jrpooler@users.noreply.github.com>
2026-04-10 14:19:29 -0700

    Update control panel

---
+++ ControlPanel/control-panel-data.js
@@ -0,0 +1,25 @@
+const controlPanelData = [
+  {
+    category: "Google",
+    items: [
+      { label: "Gmail", url: "https://mail.google.com" },
+      { label: "Contacts", url: "https://contacts.google.com" },
+      { label: "Calendar", url: "https://calendar.google.com" },
+      { label: "Maps", url: "https://www.google.com/maps" },
+      {
+        label: "Google Remote Desktop",
+        url: "https://remotedesktop.google.com/support/",
+      },
+    ],
+  },
+  {
+    category: "Photography",
+    items: [
+      {
+        label: "Bridge Tools Website",
+        url: "https://jrpooler.github.io/PhotographyHub/",
+      },
+      { label: "SmugMug Website", url: "https://www.smugmug.com/" },
+    ],
+  },
+];
